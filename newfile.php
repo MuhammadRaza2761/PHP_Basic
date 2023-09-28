e98311a68b290a27ffbb058decbc757dfb61fcd0
Muhammad Raza <136836222+MuhammadRaza2761@users.noreply.github.com>
2023-09-28 23:35:32 +0500

    Add files via upload

---
+++ newfile.php
@@ -0,0 +1,11 @@
+<?php
+$num1=15;
+$num2=13;
+
+echo "sum".$num1."+".$num2."is".$num1+$num2."<br>";
+echo "substration".$num1."+".$num2."is".$num1-$num2."<br>"; 
+echo "Multiplication".$num1."+".$num2."is".$num1*$num2."<br>"; 
+echo "divion".$num1."+".$num2."is".$num1/$num2."<br>"; 
+
+?>
+
